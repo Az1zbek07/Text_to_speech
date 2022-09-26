e2bbfc1fdda0fdf972aa922db46c5dd7dbe12159
Azizbek <muminjonovazizbek0@gmail.com>
2022-09-26 16:33:40 +0500

    Finished

--- app/src/main/java/com/example/texttospitch/SecondActivity.kt
+++ app/src/main/java/com/example/texttospitch/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -16,14 +17,15 @@ class SecondActivity : AppCompatActivity() {
     private lateinit var imageVoice: ImageView
     private lateinit var textTalk: TextView
     private lateinit var editText: EditText
+    private var textToSpeech: TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        speechToText()
+        speechToText1()
     }
 
-    private fun speechToText(){
+    private fun speechToText1(){
         imageVoice = findViewById(R.id.imageVoice)
         textTalk = findViewById(R.id.textTalk)
         imageVoice.setOnClickListener {
@@ -45,7 +47,23 @@ class SecondActivity : AppCompatActivity() {
             if (requestCode == RESULT_OK || data != null){
                 val result: ArrayList<String> = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)!!
                 textTalk.text = result[0]
+                textToSpeech(result[0])
             }
         }
+    }
+    private fun textToSpeech(text: String){
+        textToSpeech = TextToSpeech(this){ status ->
+            if (status == TextToSpeech.SUCCESS){
+                textToSpeech?.language = Locale.JAPANESE
+                textToSpeech?.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        textToSpeech = null
+        textToSpeech?.stop()
+        textToSpeech?.shutdown()
     }
 }
